1d8117a584e78b1a1df9b280baf27305424ef910
Jiu-xiao <2592509183@qq.com>
2025-10-11 18:41:27 +0800

    更新装甲板图片，调整相机参数以及识别参数

--- User/xrobot_main.hpp
+++ User/xrobot_main.hpp
@@ -21,9 +21,9 @@ static void XRobotMain(LibXR::HardwareContainer &hw) {
   static WebotsReferee WebotsReferee_0(hw, appmgr, 30.0);
   static WebotsGimbal WebotsGimbal_0(hw, appmgr);
   static WebotsFireNotify WebotsFireNotify_0(hw, appmgr);
-  static WebotsCamera WebotsCamera_0(hw, appmgr, {1440, 1080, 4320, 0, CameraBase::Encoding::RGB8, {2340.46464112537, 0.0, 713.3224120377864, 0.0, 2336.8745144649124, 547.4106752074272, 0.0, 0.0, 1.0}, CameraBase::DistortionModel::PLUMB_BOB, {-0.09558691800515781, 0.3013704144837407, -0.0008218465102445683, 0.00024582434306615617, 0.0}, {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0}, {2323.906982421875, 0.0, 712.9446224841959, 0.0, 0.0, 2324.767578125, 546.6426169058832, 0.0, 0.0, 0.0, 1.0, 0.0}}, {"camera", 30, 0.2, 0.0});
-  static ArmorDetector ArmorDetector_0(hw, appmgr, {{{ArmorNumber::NEGATIVE}, 0.5}, 0, 85, {0.1, 0.4, 40.0}, {0.7, 0.8, 3.2, 3.2, 5.5, 35.0}});
-  static ArmorTracker ArmorTracker_0(hw, appmgr, {{10.0}, {0.15, 1.0}, {5, 0.3}, {0.092, 100, 0.19133, 0.21265}, {20.0, 100.0, 800}, {0.05, 0.02}, {{0.0, 0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}});
+  static WebotsCamera WebotsCamera_0(hw, appmgr, {800, 600, 2400, 0, CameraBase::Encoding::RGB8, {1300.258730617794, 0.0, 396.2904, 0.0, 1300.258730617794, 304.1172, 0.0, 0.0, 1.0}, CameraBase::DistortionModel::PLUMB_BOB, {-0.09558691800515781, 0.3013704144837407, -0.0008218465102445683, 0.00024582434306615617, 0.0}, {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0}, {2323.906982421875, 0.0, 712.9446224841959, 0.0, 0.0, 2324.767578125, 546.6426169058832, 0.0, 0.0, 0.0, 1.0, 0.0}}, {"camera", 30, 2.0, 0.0});
+  static ArmorDetector ArmorDetector_0(hw, appmgr, {{{ArmorNumber::NEGATIVE}, 0.5}, 0, 85, {0.05, 0.6, 60.0}, {0.5, 0.7, 3.2, 3.2, 5.8, 35.0}});
+  static ArmorTracker ArmorTracker_0(hw, appmgr, {{10.0, 10.0}, {0.3, 1.0}, {5, 0.3}, {1e-08, 0, 0.0, 0.0}, {20.0, 100.0, 800}, {0.05, 0.02}, {{0.5, -0.5, 0.5, -0.5}, {0.0, 0.0, 0.0}}});
   static SharedTopic SharedTopic_Host(hw, appmgr, "uart_host", 8192, 256, {{"bullet_speed", "referee"}, {"rotation", "gimbal"}});
   static SharedTopicClient SharedTopicClient_Host(hw, appmgr, "uart_host", 8192, 256, {{"fire_notify", "tracker"}, {"target_eulr", "tracker"}});
   static SharedTopic SharedTopic_MCU(hw, appmgr, "uart_client", 8192, 256, {"fire_notify", "target_eulr"});
