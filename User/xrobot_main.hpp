2d0816e345853cfd3ee509829bed79c91a6f6f85
BandieraRossa <3132716198@qq.com>
2026-03-31 00:58:17 +0100

    修正 Webots 弹道表路径

--- User/xrobot_main.hpp
+++ User/xrobot_main.hpp
@@ -3,15 +3,13 @@
 
 // Module headers
 #include "WebotsReferee.hpp"
-#include "WebotsCamera.hpp"
-#include "BlinkLED.hpp"
-#include "SharedTopicClient.hpp"
-#include "CameraBase.hpp"
 #include "WebotsGimbal.hpp"
-#include "ArmorDetector.hpp"
-#include "SharedTopic.hpp"
-#include "ArmorTracker.hpp"
 #include "WebotsFireNotify.hpp"
+#include "WebotsCamera.hpp"
+#include "ArmorDetector.hpp"
+#include "ArmorTracker.hpp"
+#include "SharedTopic.hpp"
+#include "SharedTopicClient.hpp"
 
 static void XRobotMain(LibXR::HardwareContainer &hw) {
   using namespace LibXR;
@@ -22,8 +20,8 @@ static void XRobotMain(LibXR::HardwareContainer &hw) {
   static WebotsGimbal WebotsGimbal_0(hw, appmgr);
   static WebotsFireNotify WebotsFireNotify_0(hw, appmgr);
   static WebotsCamera WebotsCamera_0(hw, appmgr, {800, 600, 2400, 0, CameraBase::Encoding::RGB8, {1300.258730617794, 0.0, 396.2904, 0.0, 1300.258730617794, 304.1172, 0.0, 0.0, 1.0}, CameraBase::DistortionModel::PLUMB_BOB, {-0.09558691800515781, 0.3013704144837407, -0.0008218465102445683, 0.00024582434306615617, 0.0}, {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0}, {2323.906982421875, 0.0, 712.9446224841959, 0.0, 0.0, 2324.767578125, 546.6426169058832, 0.0, 0.0, 0.0, 1.0, 0.0}}, {"camera", 30, 2.0, 0.0});
-  static ArmorDetector ArmorDetector_0(hw, appmgr, {{{ArmorNumber::NEGATIVE}, 0.5}, 0, 85, {0.05, 0.6, 60.0}, {0.5, 0.7, 3.2, 3.2, 5.8, 35.0}});
-  static ArmorTracker ArmorTracker_0(hw, appmgr, {{10.0, 10.0}, {0.3, 1.0}, {5, 0.3}, {1e-08, 0, 0.0, 0.0}, {20.0, 100.0, 800}, {0.05, 0.02}, {{0.5, -0.5, 0.5, -0.5}, {0.0, 0.0, 0.0}}});
+  static ArmorDetector ArmorDetector_0(hw, appmgr, {{{ArmorNumber::NEGATIVE}, 0.5}, 0, 85, {0.05, 0.6, 48.0}, {0.5, 0.7, 3.2, 3.2, 5.8, 35.0}});
+  static ArmorTracker ArmorTracker_0(hw, appmgr, {{10.0, 10.0}, {0.3, 1.0}, {5, 0.3}, {1e-08, 0, 0.0, 0.0, SolveTrajectory::CalculateMode::TABLE_LOOKUP, {13.0, 0.0, 1.0, -1.0, 0.01, "Modules/ArmorTracker/table.bin"}}, {20.0, 100.0, 800}, {0.05, 0.02}, {{0.5, -0.5, 0.5, -0.5}, {0.0, 0.0, 0.0}}});
   static SharedTopic SharedTopic_Host(hw, appmgr, "uart_host", 8192, 256, {{"bullet_speed", "referee"}, {"rotation", "gimbal"}});
   static SharedTopicClient SharedTopicClient_Host(hw, appmgr, "uart_host", 8192, 256, {{"fire_notify", "tracker"}, {"target_eulr", "tracker"}});
   static SharedTopic SharedTopic_MCU(hw, appmgr, "uart_client", 8192, 256, {"fire_notify", "target_eulr"});
