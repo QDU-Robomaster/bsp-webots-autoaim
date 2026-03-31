4b20fddbfb75ddf285825860321f5a9f54165bfb
Jiu-xiao <Cong.Liu_Xiao@outlook.com>
2026-03-31 10:18:29 +0100

    整理 Webots 装配层开关与命名

--- User/xrobot_main.hpp
+++ User/xrobot_main.hpp
@@ -17,17 +17,17 @@ static void XRobotMain(LibXR::HardwareContainer &hw) {
   ApplicationManager appmgr;
 
   // Auto-generated module instantiations
-  static WebotsReferee WebotsReferee_0(hw, appmgr, 30.0);
-  static WebotsGimbal WebotsGimbal_0(hw, appmgr);
-  static WebotsFireNotify WebotsFireNotify_0(hw, appmgr);
-  static WebotsCamera WebotsCamera_0(hw, appmgr, {800, 600, 2400, 0, CameraBase::Encoding::RGB8, {1300.258730617794, 0.0, 396.2904, 0.0, 1300.258730617794, 304.1172, 0.0, 0.0, 1.0}, CameraBase::DistortionModel::PLUMB_BOB, {-0.09558691800515781, 0.3013704144837407, -0.0008218465102445683, 0.00024582434306615617, 0.0}, {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0}, {2323.906982421875, 0.0, 712.9446224841959, 0.0, 0.0, 2324.767578125, 546.6426169058832, 0.0, 0.0, 0.0, 1.0, 0.0}}, {"camera", 30, 2.0, 0.0});
-  static ArmorDetector ArmorDetector_0(hw, appmgr, {0, {85.0, 48.0, 0.05, 0.6, 8.0, 0.5, 5.8, 3.2, 35.0}, {false, 0, 0, 800, 600, true, 0.7, 0.3, 0.8}, {false, false, 1, 0.75}});
-  static ArmorTracker ArmorTracker_0(hw, appmgr, {{10.0, 1.0}, {5, 15, 75}, {{0.5, -0.5, 0.5, -0.5}, {0.0, 0.0, 0.0}}, {false, 1, 0.75, true}});
-  static Aimer Aimer_0(hw, appmgr, {0.0, 0.0, 60.0, 20.0, 2.0, 0.03, 0.015, 30.0, 14.0, 3.0, 2.0, 2.0, true});
-  static SharedTopic SharedTopic_Host(hw, appmgr, "uart_host", 8192, 256, {{"bullet_speed", "referee"}, {"rotation", "gimbal"}});
-  static SharedTopicClient SharedTopicClient_Host(hw, appmgr, "uart_host", 8192, 256, {{"send", "tracker"}, {"target_eulr", "tracker"}});
-  static SharedTopic SharedTopic_MCU(hw, appmgr, "uart_client", 8192, 256, {"send", "target_eulr"});
-  static SharedTopicClient SharedTopicClient_MCU(hw, appmgr, "uart_client", 8192, 256, {"bullet_speed", "rotation"});
+  static WebotsReferee webots_referee(hw, appmgr, 30.0);
+  static WebotsGimbal webots_gimbal(hw, appmgr);
+  static WebotsFireNotify webots_fire_notify(hw, appmgr);
+  static WebotsCamera webots_camera(hw, appmgr, {800, 600, 2400, 0, CameraBase::Encoding::RGB8, {1300.258730617794, 0.0, 396.2904, 0.0, 1300.258730617794, 304.1172, 0.0, 0.0, 1.0}, CameraBase::DistortionModel::PLUMB_BOB, {-0.09558691800515781, 0.3013704144837407, -0.0008218465102445683, 0.00024582434306615617, 0.0}, {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0}, {2323.906982421875, 0.0, 712.9446224841959, 0.0, 0.0, 2324.767578125, 546.6426169058832, 0.0, 0.0, 0.0, 1.0, 0.0}}, {"camera", 30, 2.0, 0.0});
+  static ArmorDetector armor_detector(hw, appmgr, {0, {85.0, 48.0, 0.05, 0.6, 8.0, 0.5, 5.8, 3.2, 35.0}, {false, 0, 0, 800, 600, true, 0.7, 0.3, 0.8}, {false, false, 1, 0.75}});
+  static ArmorTracker armor_tracker(hw, appmgr, {{10.0, 1.0}, {5, 15, 75}, {{0.5, -0.5, 0.5, -0.5}, {0.0, 0.0, 0.0}}, {false, 1, 0.75, true}});
+  static Aimer aimer(hw, appmgr, {0.0, 0.0, 60.0, 20.0, 2.0, 0.03, 0.015, 30.0, 14.0, 3.0, 2.0, 2.0, true});
+  static SharedTopic shared_topic_host(hw, appmgr, "uart_host", 8192, 256, {{"bullet_speed", "referee"}, {"rotation", "gimbal"}});
+  static SharedTopicClient shared_topic_client_host(hw, appmgr, "uart_host", 8192, 256, {{"send", "tracker"}, {"target_eulr", "tracker"}});
+  static SharedTopic shared_topic_mcu(hw, appmgr, "uart_client", 8192, 256, {"send", "target_eulr"});
+  static SharedTopicClient shared_topic_client_mcu(hw, appmgr, "uart_client", 8192, 256, {"bullet_speed", "rotation"});
 
   while (true) {
     appmgr.MonitorAll();
