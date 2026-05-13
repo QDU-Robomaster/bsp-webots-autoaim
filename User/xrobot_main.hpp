c4e2bca13574a71eabf0cc608820d313212d2f59
Xiao <Cong.Liu_Xiao@outlook.com>
2026-05-13 04:03:06 +0100

    config(webots): configure aimer inline preview

--- User/xrobot_main.hpp
+++ User/xrobot_main.hpp
@@ -51,10 +51,11 @@ static void XRobotMain(LibXR::HardwareContainer &hw) {
       {{false, -1, 2, 15, 75, 1}, {15.0, 0.135, 0.225, 0.056}, {false, "armor_tracker_preview", 0.5, 1, 1, "window", "0.0.0.0", 8080, "armor_tracker", 30.0}},
       CameraFrameSync_0
   );
-  static Aimer aimer(
+  static Aimer<ProjectConstexpr::MainCameraInfo> aimer(
       hw,
       appmgr,
-      {-1.0, -1.4, 2.0, 23.0, 14.0, true, 0.0, 0.0, 0.0, 0.0, 0.0, 0.015, 0.03, 0.135, 0.055, 0.015, 0.003, 0.05, false, true, 0.05, 100.0, 50.0, 1.0, 1.0, 100.0, 50.0, 1.0, 1.0, 10}
+      {-1.0, -1.4, 2.0, 23.0, 14.0, true, 0.0, 0.0, 0.0, 0.0, 0.0, 0.015, 0.03, 0.135, 0.055, 0.015, 0.003, 0.05, false, true, 0.05, 100.0, 50.0, 1.0, 1.0, 100.0, 50.0, 1.0, 1.0, 10, {false, "aimer_preview", 0.5, 1, 1, "window", "0.0.0.0", 8080, "aimer_preview", 30.0}, 15.0, 20},
+      CameraFrameSync_0
   );
 
   while (true) {
